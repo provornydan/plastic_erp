6d1d78b16cb98b276d5544cf10b0246e3feb7d6b
Daniil Provornii <provornydan@gmail.com>
2024-11-22 15:47:59 +0000

    Add get by id for products

--- plastic-django/stock/api/product_api.py
+++ plastic-django/stock/api/product_api.py
@@ -9,3 +9,8 @@ product_router = Router()
 @product_router.get("/", response=list[ProductSchema])
 def get_devices(request):
     return Product.objects.all()
+
+@product_router.get("/{product_id}/", response=ProductSchema)
+def get_device(request, product_id: str):
+    product = get_object_or_404(Product, id=product_id)
+    return product
